ef320620ec7ace8f07590d164f4b807a2e2c387c
Tim Humphries <tim@utf8.me>
2018-02-23 22:08:47 -0800

    Poke xmobar somewhat

--- src/XAlternative.hs
+++ src/XAlternative.hs
@@ -12,13 +12,16 @@ import           XMonad (X, XConfig (..), Layout, KeyMask, KeySym)
 import qualified XMonad as X
 import           XMonad.Layout (Choose, Tall, Mirror, Full)
 
-import           XMonad.Hooks.DynamicLog (xmobar)
+import           XMonad.Actions.DwmPromote (dwmpromote)
+import           XMonad.Hooks.DynamicLog (statusBar, sjanssenPP, )
+import           XMonad.Hooks.ManageDocks (AvoidStruts)
+import           XMonad.Layout.LayoutModifier (ModifiedLayout)
 import           XMonad.Util.CustomKeys (customKeys)
 
 
 xAlternative :: IO ()
 xAlternative =
-  X.launch =<< xmobar xConfig
+  X.launch =<< xMobar xConfig
 
 type Layouts = Choose Tall (Choose (Mirror Tall) Full)
 
@@ -37,4 +40,12 @@ xKeys =
       ((mm, xF86XK_MonBrightnessDown), X.spawn "backlight down")
     , ((mm, xF86XK_MonBrightnessUp), X.spawn "backlight up")
     , ((mm .|. shiftMask, xK_r), X.restart "xalt" True)
+    , ((mm, xK_Return), dwmpromote)
     ]
+
+xMobar :: XConfig Layouts -> IO (XConfig (ModifiedLayout AvoidStruts Layouts))
+xMobar =
+  statusBar "xmobar" sjanssenPP toggleStrutsKey
+
+toggleStrutsKey :: XConfig t -> (KeyMask, KeySym)
+toggleStrutsKey XConfig{modMask = modm} = (modm, xK_b )
