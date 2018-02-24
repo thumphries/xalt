07438dbdf4a1861e0695754d7fd5fc827c1f2950
Tim Humphries <tim@utf8.me>
2018-02-23 20:56:01 -0800

    Add config

--- src/XAlternative.hs
+++ src/XAlternative.hs
@@ -2,9 +2,22 @@
 module XAlternative where
 
 
+import           XMonad (XConfig (..))
 import qualified XMonad
+import           XMonad.Layout (Choose, Tall, Mirror, Full)
 import           XMonad.Hooks.DynamicLog (xmobar)
+
 
 xAlternative :: IO ()
 xAlternative =
-  XMonad.launch =<< xmobar XMonad.def
+  XMonad.launch =<< xmobar xConfig
+
+type Layout = Choose Tall (Choose (Mirror Tall) Full)
+
+xConfig :: XConfig Layout
+xConfig =
+  XMonad.def {
+      terminal = "urxvtc"
+    , modMask = XMonad.mod4Mask
+    , borderWidth = 3
+    }
