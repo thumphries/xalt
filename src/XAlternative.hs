8480aff30a8b33cf0e7ff803f9237210fd53fc88
Tim Humphries <tim@utf8.me>
2018-02-23 20:45:53 -0800

    Add basic xmobar

--- src/XAlternative.hs
+++ src/XAlternative.hs
@@ -1,9 +1,10 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module XAlternative where
 
 
 import qualified XMonad
-
+import           XMonad.Hooks.DynamicLog (xmobar)
 
 xAlternative :: IO ()
 xAlternative =
-  XMonad.launch XMonad.def
+  XMonad.launch =<< xmobar XMonad.def
