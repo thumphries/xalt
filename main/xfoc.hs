54f2705ab661818dd57386ed68abae13a525e03a
Tim Humphries <tim@utf8.me>
2019-07-05 19:05:12 -0700

    API refactor

--- main/xfoc.hs
+++ main/xfoc.hs
@@ -9,7 +9,11 @@ import qualified Chronos
 import           Control.Concurrent (threadDelay)
 import           Control.Concurrent.Async (Async)
 import qualified Control.Concurrent.Async as A
+import qualified Control.Concurrent.STM as STM
+import           Control.Concurrent.STM.TMVar (TMVar)
+import qualified Control.Concurrent.STM.TMVar as TMVar
 
+import           Data.Maybe (fromJust)
 import           Data.Text (Text)
 import qualified Data.Text.IO as T
 
@@ -18,32 +22,121 @@ import qualified System.Clock as Clock
 
 main :: IO ()
 main = do
-  rt <-
-    runTask $
-      Task {
-          taskName = TaskName "First Task"
-        , taskDuration = Chronos.minute
-        }
+  api <- newAPI
 
+  _ <-
+    apiSubmit api Task {
+        taskName = TaskName "Second Task"
+      , taskDuration = Chronos.minute
+      }
+
+  subscribe api
+
+subscribe :: API -> IO ()
+subscribe api = do
   let
     poll = do
       delayTimespan Chronos.second
-      status <- runningTaskPoll rt
+      StatusResponse task _started status <- fromJust <$> apiStatus api -- FIXME
       case status of
         StatusComplete ->
           pure ResultComplete
         StatusRunning elapsed -> do
           T.putStrLn $
-               unTaskName (taskName (runningTask rt))
+               unTaskName (taskName task)
             <> ": "
             <> renderTimespan
-                 (taskDuration (runningTask rt) `timespanDifference` elapsed)
+                 (taskDuration task `timespanDifference` elapsed)
           poll
-
-  poller <- A.async poll
-  (_, result) <- A.waitAnyCancel [runningTaskThread rt, poller]
+  result <- A.race poll (apiWait api)
   print result
 
+-- ---------------------------------------------------------------------------
+-- Stateful API
+
+data API =
+  API {
+      apiSubmit :: Task -> IO (Maybe SubmitResponse)
+    , apiStatus :: IO (Maybe StatusResponse)
+    , apiWait :: IO (Maybe TaskResult)
+--    , stop :: Reason -> IO ()
+    }
+
+data SubmitResponse =
+  SubmitResponse {
+      submitTask :: Task
+    , submitTaskStarted :: Time
+    } deriving (Eq, Ord, Show)
+
+data StatusResponse =
+  StatusResponse {
+      statusTask :: Task
+    , statusTaskStarted :: Time
+    , statusTaskStatus :: TaskStatus
+    } deriving (Eq, Ord, Show)
+
+newAPI :: IO API
+newAPI = do
+  st <- newState
+  pure API {
+      apiSubmit = submit' st
+    , apiStatus = status' st
+    , apiWait = wait' st
+    }
+
+type State = TMVar (Maybe RunningTask)
+
+newState :: IO State
+newState =
+  TMVar.newTMVarIO Nothing
+
+submit' :: State -> Task -> IO (Maybe SubmitResponse)
+submit' st t = do
+  v <- STM.atomically $ TMVar.takeTMVar st
+  case v of
+    Just _task -> do
+      STM.atomically $ TMVar.putTMVar st v
+      pure Nothing
+    Nothing -> do
+      rt <- runTask t
+      STM.atomically $ TMVar.putTMVar st (Just rt)
+      pure . Just $
+        SubmitResponse {
+            submitTask = runningTask rt
+          , submitTaskStarted = runningTaskStarted rt
+          }
+
+status' :: State -> IO (Maybe StatusResponse)
+status' st = do
+  mv <- STM.atomically $ TMVar.tryReadTMVar st
+  case mv of
+    Just (Just rt) -> do
+      ts <- runningTaskPoll rt
+      pure . Just $
+        StatusResponse {
+            statusTask = runningTask rt
+          , statusTaskStarted = runningTaskStarted rt
+          , statusTaskStatus = ts
+          }
+    _ ->
+      pure Nothing
+
+wait' :: State -> IO (Maybe TaskResult)
+wait' st = do
+  mv <- STM.atomically $ TMVar.tryReadTMVar st
+  case mv of
+    Just (Just rt) ->
+      fmap Just (A.wait (runningTaskThread rt))
+    _ ->
+      pure Nothing
+
+-- ---------------------------------------------------------------------------
+-- Event loop
+
+
+
+-- ---------------------------------------------------------------------------
+-- Task
 
 data Task =
   Task {
@@ -97,6 +190,10 @@ runTask t@(Task _name duration) = do
     , runningTaskPoll = poll
     , runningTaskThread = thread
     }
+
+stopTask :: RunningTask -> IO ()
+stopTask rt =
+  A.cancel (runningTaskThread rt)
 
 checkElapsed :: Timespan -> Clock.TimeSpec -> (Timespan -> IO a) -> IO a -> IO a
 checkElapsed duration t0 running done = do
--- xalternative.nix
+++ xalternative.nix
@@ -1,6 +1,6 @@
 { mkDerivation, async, base, chronos, clock, config-value
-, containers, directory, gi-gtk, gi-gtk-hs, gtk3, microlens, mtl
-, optparse-applicative, stdenv, taffybar, text, torsor
+, containers, dbus, directory, gi-gtk, gi-gtk-hs, gtk3, microlens
+, mtl, optparse-applicative, stdenv, stm, taffybar, text
 , transformers, X11, xmonad, xmonad-contrib
 }:
 mkDerivation {
@@ -14,8 +14,8 @@ mkDerivation {
     transformers X11 xmonad xmonad-contrib
   ];
   executableHaskellDepends = [
-    async base chronos clock gi-gtk gi-gtk-hs gtk3 mtl
-    optparse-applicative taffybar text torsor transformers
+    async base chronos clock dbus gi-gtk gi-gtk-hs gtk3 mtl
+    optparse-applicative stm taffybar text transformers
   ];
   homepage = "https://github.com/thumphries/xalternative";
   description = "XMonad+";
