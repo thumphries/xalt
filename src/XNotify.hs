ce685f320a2c6a8c9e162df8123942c9ab6c8a0b
Tim Humphries <tim@utf8.me>
2019-07-21 20:49:15 -0700

    Clean up notification code

--- src/XNotify.hs
+++ src/XNotify.hs
@@ -1,6 +1,6 @@
 -- | FreeDesktop.org Desktop Notifications.
 --
--- Forked from fdo-notify.
+-- Forked from fdo-notify 2019-07-21.
 --
 -- Copyright (c) 2009, Max Rabkin
 --
@@ -61,211 +61,337 @@ module XNotify
     ) where
 
 
-import           Data.Bifunctor (second)
 import qualified Data.Map.Strict as M
 import           Data.Maybe (fromMaybe, fromJust)
-import           Data.Int
-import           Data.Word
-import           Data.Char (isLower, toLower)
+import           Data.Int (Int32)
+import           Data.Word (Word32, Word8)
 
 import           DBus
 import           DBus.Client
 
 
-
 -- |Contents of a notification
-data Note = Note { appName :: String
-                 , appImage :: Maybe Icon
-                 , summary :: String
-                 , body :: Maybe Body
-                 , actions :: [(Action, String)]
-                 , hints :: [Hint]
-                 , expiry :: Timeout
-                 }
-    deriving (Eq, Show)
+data Note =
+  Note {
+      appName :: String
+    , appImage :: Maybe Icon
+    , summary :: String
+    , body :: Maybe Body
+    , actions :: [(Action, String)]
+    , hints :: [Hint]
+    , expiry :: Timeout
+    } deriving (Eq, Show)
 
 -- |A 'Note' with default values.
 -- All fields are blank except for 'expiry', which is 'Dependent'.
 blankNote :: Note
-blankNote = Note { appName=""
-                   , appImage=Nothing
-                   , summary=""
-                   , body=Nothing
-                   , actions=[]
-                   , hints=[]
-                   , expiry=Dependent
-                   }
+blankNote =
+  Note {
+      appName = ""
+    , appImage = Nothing
+    , summary = ""
+    , body = Nothing
+    , actions = []
+    , hints = []
+    , expiry = Dependent
+    }
 
 -- |Message bodies may contain simple markup.
 -- NotifyOSD doesn't support any markup.
-data Body =   Text String
-            | Bold Body
-            | Italic Body
-            | Underline Body
-            | Hyperlink URL Body
-            | Img URL String
-            | Concat Body Body
+data Body =
+    Text String
+  | Bold Body
+  | Italic Body
+  | Underline Body
+  | Hyperlink URL Body
+  | Img URL String
+  | Concat Body Body
     deriving (Eq, Show)
 
 type URL = String
 
--- |Length of time to display notifications. NotifyOSD seems to ignore these.
-data Timeout =   Never              -- ^Wait to be dismissed by user
-               | Dependent          -- ^Let the notification service decide
-               | Milliseconds Int32 -- ^Show notification for a fixed duration
-                                    -- (must be positive)
-    deriving (Eq, Show)
+-- | Length of time to display notifications. NotifyOSD seems to ignore these.
+data Timeout =
+    Never
+  -- ^ Wait to be dismissed by user
+  | Dependent
+  -- ^ Let the notification service decide
+  | Milliseconds Int32 {- FIXME Natural -}
+  -- ^ Show notification for a fixed duration
+  deriving (Eq, Show)
 
-newtype Action = Action { actionName :: String }
-    deriving (Eq, Show)
+newtype Action =
+  Action {
+      actionName :: String
+    } deriving (Eq, Show)
 
--- |Images are not yet supported
-newtype Image = Image { bitmap :: String }
-    deriving (Eq, Show)
+-- | Images are not yet supported
+newtype Image =
+  Image {
+      bitmap :: String
+    } deriving (Eq, Show)
 
--- |An Icon is either a path to an image, or a name in an icon theme
-data Icon = File FilePath | Icon String
-    deriving (Eq, Show)
+-- | An Icon is either a path to an image, or a name in an icon theme
+data Icon =
+    File FilePath
+  | Icon String
+  deriving (Eq, Show)
 
 iconString :: Icon -> String
-iconString (File fp) = "file://" ++ fp
-iconString (Icon name) = name
+iconString icon =
+  case icon of
+    File fp ->
+      "file://" ++ fp
+    Icon name ->
+      name
 
--- |Urgency of the notification. Notifications may be prioritised by urgency.
-data UrgencyLevel =   Low
-                    | Normal
-                    | Critical -- ^Critical notifications require user attention
-    deriving (Eq, Ord, Enum, Show)
+-- | Urgency of the notification. Notifications may be prioritised by urgency.
+data UrgencyLevel =
+    Low
+  | Normal
+  | Critical
+  deriving (Eq, Ord, Enum, Show)
 
--- |Various hints about how the notification should be displayed
-data Hint =   Urgency UrgencyLevel
-            | Category Category
-            -- DesktopEntry ApplicationDesktopID
-            | ImageData Image
-            | ImagePath Icon
-            | SoundFile FilePath
-            | SuppressSound Bool
-            | X Int32
-            | Y Int32
-    deriving (Eq, Show)
+-- | Various hints about how the notification should be displayed
+data Hint =
+    Urgency UrgencyLevel
+  | Category Category
+  -- DesktopEntry ApplicationDesktopID
+  | ImageData Image
+  | ImagePath Icon
+  | SoundFile FilePath
+  | SuppressSound Bool
+  | X Int32
+  | Y Int32
+  deriving (Eq, Show)
 
--- |Categorisation of (some) notifications
-data Category =   Device | DeviceAdded | DeviceError | DeviceRemoved
-                | Email | EmailArrived | EmailBounced
-                | Im | ImError | ImReceived
-                | Network | NetworkConnected | NetworkDisconnected | NetworkError
-                | Presence | PresenceOffline | PresenceOnline
-                | Transfer | TransferComplete | TransferError
-    deriving (Eq, Show)
+-- | Categorisation of (some) notifications
+data Category =
+    Device
+  | DeviceAdded
+  | DeviceError
+  | DeviceRemoved
+  | Email
+  | EmailArrived
+  | EmailBounced
+  | Im
+  | ImError
+  | ImReceived
+  | Network
+  | NetworkConnected
+  | NetworkDisconnected
+  | NetworkError
+  | Presence
+  | PresenceOffline
+  | PresenceOnline
+  | Transfer
+  | TransferComplete
+  | TransferError
+  deriving (Eq, Show)
 
-data ClosedReason = Expired | Dismissed | CloseNotificationCalled
-data NotificationEvent = ActionInvoked Action | Closed ClosedReason
+data ClosedReason =
+    Expired
+  | Dismissed
+  | CloseNotificationCalled
+  deriving (Eq, Show)
 
--- |A handle on a displayed notification
+data NotificationEvent =
+    ActionInvoked Action
+  | Closed ClosedReason
+  deriving (Eq, Show)
+
+-- | A handle on a displayed notification.
+--
 -- The notification may not have reached the screen yet, and may already have
 -- been closed.
-data Notification = Notification { notificationId :: Word32 }
+data Notification =
+  Notification {
+      notificationId :: Word32
+    } deriving (Eq, Show)
 
--- |Display a notification.
--- Return a handle which can be used to replace the notification.
+-- | Display a notification.
+--
+-- Returns a handle which can be used to replace the notification.
 notify :: Client -> Note -> IO Notification
-notify cl = replace cl (Notification { notificationId=0 })
+notify cl =
+  replace cl (Notification { notificationId = 0 })
 
 callNotificationMethod :: Client -> MemberName -> [Variant] -> IO MethodReturn
 callNotificationMethod client methodName_ args =
-    call_ client $ (methodCall path iface methodName_)
-        { methodCallDestination=Just busname
-        , methodCallBody=args
-        }
-    where
-        busname = "org.freedesktop.Notifications"
-        path = "/org/freedesktop/Notifications"
-        iface = "org.freedesktop.Notifications"
+  call_ client $
+    (methodCall path iface methodName_) {
+        methodCallDestination = Just busname
+      , methodCallBody = args
+      }
+  where
+    busname = "org.freedesktop.Notifications"
+    path = "/org/freedesktop/Notifications"
+    iface = "org.freedesktop.Notifications"
 
--- |Replace an existing notification.
+-- | Replace an existing notification.
+--
 -- If the notification has already been closed, a new one will be created.
 replace :: Client -> Notification -> Note -> IO Notification
 replace cl (Notification { notificationId=replaceId }) note =
-    Notification . fromJust . fromVariant . head . methodReturnBody <$>
-        callNotificationMethod cl "Notify" args
-    where
-        args = map ($ note)
-            [ toVariant . appName
-               , const $ toVariant (replaceId::Word32)
-               , toVariant . fromMaybe "" . fmap iconString . appImage
-               , toVariant . summary
-               , toVariant . fromMaybe "" . fmap flattenBody . body
-               , toVariant . actionsArray . actions
-               , toVariant . hintsDict . hints
-               , toVariant . timeoutInt . expiry
-               ]
+  -- FIXME just brutal stuff
+  Notification . fromJust . fromVariant . head . methodReturnBody <$>
+      callNotificationMethod cl "Notify" args
+  where
+    args =
+      map ($ note) [
+          toVariant . appName
+        , const $ toVariant (replaceId::Word32)
+        , toVariant . fromMaybe "" . fmap iconString . appImage
+        , toVariant . summary
+        , toVariant . fromMaybe "" . fmap flattenBody . body
+        , toVariant . actionsArray . actions
+        , toVariant . hintsDict . hints
+        , toVariant . timeoutInt . expiry
+        ]
 
-data Capability =   ActionsCap | BodyCap | BodyHyperlinksCap | BodyImagesCap
-                  | BodyMarkupCap | IconMultiCap | IconStaticCap | SoundCap
-                  | UnknownCap String
-    deriving (Eq, Read, Show)
+data Capability =
+    ActionsCap
+  | BodyCap
+  | BodyHyperlinksCap
+  | BodyImagesCap
+  | BodyMarkupCap
+  | IconMultiCap
+  | IconStaticCap
+  | SoundCap
+  | UnknownCap String
+  deriving (Eq, Read, Show)
 
--- |Determine the server's capabilities
+-- | Determine the server's capabilities
 getCapabilities :: Client -> IO [Capability]
-getCapabilities cl = map readCapability . fromJust
-                    . fromVariant . head . methodReturnBody
-                    <$> callNotificationMethod cl "GetCapabilities" []
+getCapabilities cl =
+  -- FIXME double double animal style
+  map readCapability . fromJust . fromVariant . head . methodReturnBody
+    <$> callNotificationMethod cl "GetCapabilities" []
 
 readCapability :: String -> Capability
-readCapability str = case str of
-                    "actions" -> ActionsCap
-                    "body" -> BodyCap
-                    "body-hyperlinks" -> BodyHyperlinksCap
-                    "body-images" -> BodyImagesCap
-                    "body-markup" -> BodyMarkupCap
-                    "icon-multi" -> IconMultiCap
-                    "icon-static" -> IconStaticCap
-                    "sound" -> SoundCap
-                    s -> UnknownCap s
+readCapability str =
+  case str of
+    "actions" ->
+      ActionsCap
+    "body" ->
+      BodyCap
+    "body-hyperlinks" ->
+      BodyHyperlinksCap
+    "body-images" ->
+      BodyImagesCap
+    "body-markup" ->
+      BodyMarkupCap
+    "icon-multi" ->
+      IconMultiCap
+    "icon-static" ->
+      IconStaticCap
+    "sound" ->
+      SoundCap
+    s ->
+      UnknownCap s
 
 timeoutInt :: Timeout -> Int32
-timeoutInt Never = 0
-timeoutInt Dependent = -1
-timeoutInt (Milliseconds n)
-    | n > 0     = n
-    | otherwise = error "notification timeout not positive"
+timeoutInt to =
+  case to of
+    Never ->
+      0
+    Dependent ->
+      -1
+    Milliseconds n ->
+      n
+
+escape :: String -> String
+escape str =
+  concatMap escape_ str
+  where
+    escape_ '>' = "&gt;"
+    escape_ '<' = "&lt;"
+    escape_ '&' = "&amp;"
+    escape_ x = [x]
 
 flattenBody :: Body -> String
-flattenBody (Text s) = concatMap escape s
-    where
-        escape '>' = "&gt;"
-        escape '<' = "&lt;"
-        escape '&' = "&amp;"
-        escape x = [x]
-flattenBody (Bold b) = "<b>" ++ flattenBody b ++ "</b>"
-flattenBody (Italic b) = "<i>" ++ flattenBody b ++ "</i>"
-flattenBody (Underline b) = "<u>" ++ flattenBody b ++ "</u>"
-flattenBody (Hyperlink h b) = "<a href=\"" ++ h ++ "\">" ++ flattenBody b ++ "</a>"
-flattenBody (Img h alt) = "<img src=\"" ++ h ++ "\" alt=\"" ++ alt ++ "\"/>"
-flattenBody (Concat b1 b2) = flattenBody b1 ++ flattenBody b2
+flattenBody bdy =
+  case bdy of
+    Text s ->
+      escape s
+    Bold b ->
+      "<b>" ++ flattenBody b ++ "</b>"
+    Italic b ->
+      "<i>" ++ flattenBody b ++ "</i>"
+    Underline b ->
+      "<u>" ++ flattenBody b ++ "</u>"
+    Hyperlink h b ->
+      "<a href=\"" ++ h ++ "\">" ++ flattenBody b ++ "</a>"
+    Img h alt ->
+      "<img src=\"" ++ h ++ "\" alt=\"" ++ alt ++ "\"/>"
+    Concat b1 b2 ->
+      flattenBody b1 ++ flattenBody b2
 
 actionsArray :: [(Action, String)] -> [String]
-actionsArray = concatMap pairList
-    where
-        pairList (a, b) = [actionName a, b]
+actionsArray =
+  concatMap (\(a, b) -> [actionName a, b])
 
 hintsDict :: [Hint] -> M.Map String Variant
-hintsDict = M.fromList . map hint
-    where
-        hint :: Hint -> (String, Variant)
-        hint (Urgency u) = ("urgency", toVariant (fromIntegral $ fromEnum u :: Word8))
-        hint (Category c) = ("category", toVariant $ catName c)
-        hint (ImagePath p) = ("image-path", toVariant $ iconString p)
-        hint (ImageData i) = ("image-data", toVariant $ bitmap i)
-        hint (SoundFile s) = ("sound-file", toVariant s)
-        hint (SuppressSound b) = ("suppress-sound", toVariant b)
-        hint (X x) = ("x", toVariant x)
-        hint (Y y) = ("x", toVariant y)
+hintsDict hints_ =
+  M.fromList . flip fmap hints_ $ \hint ->
+    case hint of
+       Urgency u ->
+         ("urgency", toVariant (fromIntegral $ fromEnum u :: Word8))
+       Category c ->
+         ("category", toVariant (catName c))
+       ImagePath p ->
+         ("image-path", toVariant (iconString p))
+       ImageData i ->
+         ("image-data", toVariant (bitmap i))
+       SoundFile s ->
+         ("sound-file", toVariant s)
+       SuppressSound b ->
+         ("suppress-sound", toVariant b)
+       X x ->
+         ("x", toVariant x)
+       Y y ->
+         ("x", toVariant y)
 
--- HACK: Assumes the constructor for category foo.bar is FooBar and
--- categories have no capital letters
 catName :: Category -> String
-catName cat = catName' (show cat)
-    where
-        catName' (c:cs) = map toLower $ c: (uncurry (++) . second ('.':) . span isLower $ cs)
-        catName' [] = []
+catName cat =
+  case cat of
+    Device ->
+      "device"
+    DeviceAdded ->
+      "device.added"
+    DeviceError ->
+      "device.error"
+    DeviceRemoved ->
+      "device.removed"
+    Email ->
+      "email"
+    EmailArrived ->
+      "email.arrived"
+    EmailBounced ->
+      "email.bounced"
+    Im ->
+      "im"
+    ImError ->
+      "im.error"
+    ImReceived ->
+      "im.received"
+    Network ->
+      "network"
+    NetworkConnected ->
+      "network.connected"
+    NetworkDisconnected ->
+      "network.disconnected"
+    NetworkError ->
+      "network.error"
+    Presence ->
+      "presence"
+    PresenceOffline ->
+      "presence.offline"
+    PresenceOnline ->
+      "presence.online"
+    Transfer ->
+      "transfer"
+    TransferComplete ->
+      "transfer.complete"
+    TransferError ->
+      "transfer.error"
